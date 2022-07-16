664e5fec264714977381855232edef32d3de8136
jean981 <jean.sa@celfinet.com>
2022-07-16 21:46:07 +0100

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
+
 import Header from "./components/Header"
 import SalesCard from "./components/SelesCard"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return (
     <>
+    <ToastContainer />
       <Header />
       <main>
         <section id="sales">
