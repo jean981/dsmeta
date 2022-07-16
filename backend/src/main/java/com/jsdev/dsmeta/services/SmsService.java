7d1f03acbb01080d68f6bb0ba379ecb6039525fd
jean981 <jean.sa@celfinet.com>
2022-07-16 12:31:05 +0100

    Twilio SMS

---
+++ backend/src/main/java/com/jsdev/dsmeta/services/SmsService.java
@@ -0,0 +1,50 @@
+package com.jsdev.dsmeta.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.jsdev.dsmeta.entities.Sale;
+import com.jsdev.dsmeta.repositories.SaleRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@Service
+public class SmsService {
+
+	@Value("${twilio.sid}")
+	private String twilioSid;
+
+	@Value("${twilio.key}")
+	private String twilioKey;
+
+	@Value("${twilio.phone.from}")
+	private String twilioPhoneFrom;
+
+	@Value("${twilio.phone.to}")
+	private String twilioPhoneTo;
+	
+	@Autowired
+	private SaleRepository saleRepository;
+
+	public void sendSms(Long saleId) {
+
+		Sale sale = saleRepository.findById(saleId).get();
+		
+		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
+		
+		String msg = "Notice:\n The seller "+ sale.getSellerName()+" was awesome!"
+					+"\n He reached the sales target with the value of R$"+ String.format(
+					"%.2f",sale.getAmount()) + "\n So he was the highlight of the "+date;
+		
+		Twilio.init(twilioSid, twilioKey);
+
+		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+		Message message = Message.creator(to, from, msg).create();
+
+		System.out.println(message.getSid());
+	}
+}
